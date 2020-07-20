bce26ee3d814d0bc85ff8599c8c28903cf51f6f6
cogniocode <stellarandrey@gmail.com>
2020-07-20 17:21:43 +0600

    Add Axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Atomio from 'atomio-vue'
+import Axios from 'axios'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -8,6 +9,9 @@ import store from './store'
 Vue.use(Atomio)
 
 Vue.config.productionTip = false
+
+//TODO: Change to an actual IP.
+Axios.defaults.baseURL = `https://${window.location.hostname}/api`
 
 const atomio = new Atomio.Config({
     theming: {
