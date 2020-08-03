7b0a62cff241128b32b28a140ebefd010bfbde9e
cogniocode <stellarandrey@gmail.com>
2020-08-03 15:51:27 +0600

    Add base Page component

---
+++ src/components/Page.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page">
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Page"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .page {
+
+    }
+</style>
