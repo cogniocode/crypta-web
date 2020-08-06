86ca32774ff8d93390ca47dbe15ae15763eecd90
cogniocode <stellarandrey@gmail.com>
2020-08-06 20:11:06 +0600

    Add method for retrieving user from existing auth token

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,11 +1,28 @@
-import type { UserCreationDTO, UserRetrievalDTO } from "@/types/api/user"
-import { createUser } from "@/api/user"
-import { ApiError } from "@/api/util"
-import { ServiceError } from "@/services/util"
+import type {UserCreationDTO, UserRetrievalDTO} from "@/types/api/user"
+import {createUser, getUserByUsername} from "@/api/user"
+import {ApiError} from "@/api/util"
+import {ServiceError} from "@/services/util"
+import {getToken, getUsernameFromToken} from "@/services/auth"
 
 export async function signUp(userCreationData: UserCreationDTO): Promise<UserRetrievalDTO> {
     try {
         return await createUser(userCreationData)
+    } catch (e) {
+        if (e instanceof ApiError) {
+            throw new ServiceError(e.message)
+        } else throw e
+    }
+}
+
+export async function getAuthUser(): Promise<UserRetrievalDTO> {
+    try {
+        const authUsername = getUsernameFromToken()
+        const authToken = getToken()
+
+        if (authToken == null)
+            throw new ServiceError("Auth token is not present.")
+
+        return getUserByUsername(authUsername, authToken)
     } catch (e) {
         if (e instanceof ApiError) {
             throw new ServiceError(e.message)
