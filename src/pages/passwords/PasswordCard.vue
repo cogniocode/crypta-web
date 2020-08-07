b4fa3d2f751cd83843a85bfe2b51b9574ddef666
cogniocode <stellarandrey@gmail.com>
2020-08-07 18:13:18 +0600

    Add passwords page (WIP)

---
+++ src/pages/passwords/PasswordCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-card class="password-card">
+
+    </b-card>
+</template>
+
+<script>
+    export default {
+        name: "PasswordCard",
+        props: {
+            id: String
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/pages/passwords/PasswordsPage.vue
@@ -0,0 +1,46 @@
+<template>
+    <page class="passwords-page">
+        <app-layout>
+            <b-col cols="6" class="pt-3 pb-3">
+                <div class="passwords-page__header">
+                    <h3 class="passwords-page__header__title m-0 text-muted">
+                        Passwords
+                    </h3>
+                    <b-button class="passwords-page__header__create-button ml-auto" variant="primary">
+                        <b-icon icon="plus"/>
+                        Create password
+                    </b-button>
+                    <b-dropdown right class="passwords-page__header__options-dropdown ml-2">
+                        <template #button-content>
+                            <b-icon icon="three-dots-vertical" class="mr-1"/>
+                        </template>
+                        <b-dropdown-form>
+
+                        </b-dropdown-form>
+                    </b-dropdown>
+                </div>
+            </b-col>
+        </app-layout>
+    </page>
+</template>
+
+<script>
+    import Page from "@/components/Page";
+    import AppLayout from "@/components/layouts/AppLayout";
+    export default {
+        name: "PasswordsPage",
+        components: {AppLayout, Page}
+    }
+</script>
+
+<style lang="scss" scoped>
+    .passwords-page__header {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .passwords-page__header__title {
+        font-size: 2rem;
+        user-select: none;
+    }
+</style>
