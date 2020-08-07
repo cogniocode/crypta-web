d7c7aaf2fcc1b559b8d2f086e2d5d4742b9f13f4
cogniocode <stellarandrey@gmail.com>
2020-08-07 18:12:36 +0600

    Change layout of sign in page

--- src/pages/signin/SignInPage.vue
+++ src/pages/signin/SignInPage.vue
@@ -1,43 +1,44 @@
 <template>
     <page class="sign-in-page">
-        <main>
+        <guest-layout>
             <b-container class="sign-in-page__container">
-                <b-row align-h="center">
-                    <b-col lg="4" md="6" sm="12">
-                        <div class="sign-in-page__sign-in-block">
-                            <h4 class="sign-in-page__sign-in-block__title">
-                                Sign in
-                            </h4>
-                            <sign-in-form class="sign-in-page__sign-in-block__sign-in-form mb-3 mt-3"/>
-                            <b-link class="sign-in-page__sign-in-block__sign-up-link" to="/signup">Sign up</b-link>
-                        </div>
-                    </b-col>
-                </b-row>
+                <b-col lg="4" md="6" sm="12">
+                    <div class="sign-in-page__sign-in-block">
+                        <h3 class="sign-in-page__sign-in-block__title">
+                            Sign in
+                        </h3>
+                        <sign-in-form class="sign-in-page__sign-in-block__sign-in-form mb-3 mt-4"/>
+                        <b-link class="sign-in-page__sign-in-block__sign-up-link" to="/signup">Sign up</b-link>
+                    </div>
+                </b-col>
             </b-container>
-        </main>
+        </guest-layout>
     </page>
 </template>
 
 <script>
     import Page from "@/components/Page"
     import SignInForm from "@/pages/signin/SignInForm"
+    import GuestLayout from "@/components/layouts/GuestLayout";
 
     export default {
         name: "SignInPage",
-        components: {SignInForm, Page}
+        components: {GuestLayout, SignInForm, Page}
     }
 </script>
 
 <style lang="scss" scoped>
-    .sign-in-page {
+    .sign-in-page__container {
         display: flex;
         justify-content: center;
         align-items: center;
-        flex-direction: column;
+        flex-grow: 1;
     }
 
-    .sign-in-page main {
+    .sign-in-page__sign-in-block__sign-up-link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         width: 100%;
-        height: 100%;
     }
 </style>
