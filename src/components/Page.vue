6997aba6b6cb601c3b31a86de4d7cfe1177d7713
cogniocode <stellarandrey@gmail.com>
2020-08-07 16:43:57 +0600

    Fix page component styles to use flexbox

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -12,6 +12,10 @@
 
 <style lang="scss" scoped>
     .page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
         height: 100%;
         width: 100%;
         min-height: 100vh;
