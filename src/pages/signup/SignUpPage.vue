4ce065a404f389976f1c2b34eae8f53e2fe86368
cogniocode <stellarandrey@gmail.com>
2020-08-07 18:13:06 +0600

    Change layout of sign up page

--- src/pages/signup/SignUpPage.vue
+++ src/pages/signup/SignUpPage.vue
@@ -1,29 +1,36 @@
 <template>
     <page class="sign-up-page">
-        <main>
+        <guest-layout>
             <b-container class="sign-up-page__container">
-                <b-row align-h="center">
-                    <b-col lg="4" md="6" sm="12">
-                        <sign-up-form/>
-                    </b-col>
-                </b-row>
+                <b-col lg="4" md="6" sm="12">
+                    <div class="sign-up-page__sign-up-block">
+                        <h3 class="sign-up-page__sign-up-block__title">
+                            Sign up
+                        </h3>
+                        <sign-up-form class="mt-4"/>
+                    </div>
+                </b-col>
             </b-container>
-        </main>
+        </guest-layout>
     </page>
 </template>
 
 <script>
     import Page from "../../components/Page"
     import SignUpForm from "./SignUpForm";
+    import GuestLayout from "@/components/layouts/GuestLayout";
 
     export default {
         name: "SignUpPage",
-        components: {SignUpForm, Page}
+        components: {GuestLayout, SignUpForm, Page}
     }
 </script>
 
 <style lang="scss" scoped>
     .sign-up-page__container {
-        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-grow: 1;
     }
 </style>
