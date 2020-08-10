220f9cf3814447a71a295ce546697a0cda503bab
cogniocode <stellarandrey@gmail.com>
2020-08-10 22:24:03 +0600

    Add user mutation for clearing user

--- src/store/modules/user/mutations.js
+++ src/store/modules/user/mutations.js
@@ -3,5 +3,8 @@ import * as types from "./mutationTypes"
 export default {
     [types.SET_USER] (state, user) {
         state.user = user
+    },
+    [types.CLEAR_USER] (state) {
+        state.user = null
     }
 }
