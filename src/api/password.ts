c0eb2a88db88988433b04bd193a1f2824b2c708c
cogniocode <stellarandrey@gmail.com>
2020-08-11 21:50:55 +0600

    Update password API module
    Fix create function not accepting password size.
    Add method for retrieving decoded password.

--- src/api/password.ts
+++ src/api/password.ts
@@ -1,13 +1,18 @@
-import {PasswordCreationDTO, PasswordRetrievalDTO} from "@/types/api/password"
+import {DecodedPasswordDTO, PasswordCreationDTO, PasswordRetrievalDTO} from "@/types/api/password"
 import Axios from "axios"
 import {ApiError, getDefaultAxiosConfig} from "@/api/util"
 
 const passwordApiPath = (userId: number, passwordId?: number) =>
     passwordId ? `/users/${userId}/passwords/${passwordId}` : `/users/${userId}/passwords`
 
-export async function createPassword(userId: number, passwordDTO: PasswordCreationDTO, token: string): Promise<PasswordRetrievalDTO> {
+export async function createPassword(userId: number, passwordDTO: PasswordCreationDTO, token: string, size?: number): Promise<PasswordRetrievalDTO> {
     try {
-        const result = await Axios.post(passwordApiPath(userId), passwordDTO, getDefaultAxiosConfig({token}))
+        const result = await Axios.post(passwordApiPath(userId), passwordDTO, {
+            ...getDefaultAxiosConfig({token}),
+            params: {
+                size
+            }
+        })
 
         if (result.status === 201) {
             return result.data
@@ -22,6 +27,20 @@ export async function createPassword(userId: number, passwordDTO: PasswordCreati
 export async function getPasswordById(userId: number, passwordId: number, token: string): Promise<PasswordRetrievalDTO> {
     try {
         const result = await Axios.get(passwordApiPath(userId, passwordId), getDefaultAxiosConfig({token}))
+
+        if (result.status === 200) {
+            return result.data
+        } else throw new ApiError(result.data.message, result.status)
+    } catch (e) {
+        if (e.isAxiosError) {
+            throw new ApiError("Server unreachable.", 500)
+        } else throw e
+    }
+}
+
+export async function getDecodedPasswordById(userId: number, passwordId: number, token: string): Promise<DecodedPasswordDTO> {
+    try {
+        const result = await Axios.get(passwordApiPath(userId, passwordId) + ":decoded", getDefaultAxiosConfig({token}))
 
         if (result.status === 200) {
             return result.data
