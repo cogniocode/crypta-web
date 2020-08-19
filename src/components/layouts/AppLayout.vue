89034961f95f5e94ded307b1fd36281c573902ca
cogniocode <stellarandrey@gmail.com>
2020-08-19 20:54:20 +0600

    Update app layout to toggle menu on mobile

--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -1,11 +1,9 @@
 <template>
-    <div class="app-layout d-flex flex-grow-1 flex-column w-100 h-100">
-        <app-header/>
+    <div class="app-layout d-flex flex-grow-1 flex-row flex-wrap w-100 h-100">
+        <app-header class="app-layout__header"/>
+        <app-aside-menu :class="{inv: !menuVisible}" class="app-layout__aside-menu col-12 col-xl-2"/>
         <main class="app-layout__content d-flex flex-grow-1 flex-column">
             <b-container fluid="true" class="d-flex flex-grow-1">
-                <b-col cols="2" class="pr-0">
-                    <app-aside-menu class="border-right border-muted"/>
-                </b-col>
                 <slot/>
             </b-container>
         </main>
@@ -17,10 +15,47 @@
     import AppAsideMenu from "@/components/AppAsideMenu";
     export default {
         name: "AppLayout",
-        components: {AppAsideMenu, AppHeader}
+        components: {AppAsideMenu, AppHeader},
+        computed: {
+            menuVisible() {
+                return this.$store.state.menu.visible
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+    @import "~bootstrap/scss/functions";
+    @import "~bootstrap/scss/variables";
+    @import "~bootstrap/scss/mixins";
 
+    .app-layout__header {
+        position: fixed;
+        z-index: 10;
+    }
+
+    .app-layout__content {
+        margin-top: 60px;
+
+        @include media-breakpoint-up(xl) {
+            margin-left: 16.667%;
+        }
+    }
+
+    .app-layout__aside-menu  {
+        height: 100%;
+        position: fixed;
+        z-index: 10;
+        margin-top: 60px;
+
+        @include media-breakpoint-up(xl) {
+            border-right: 1px solid rgba(#000, .1);
+        }
+
+        @include media-breakpoint-down(lg) {
+            &.inv {
+                visibility: hidden;
+            }
+        }
+    }
 </style>
