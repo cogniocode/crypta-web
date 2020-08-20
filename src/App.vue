a652aba315e12f4960440a1a85ffc9a1c646a5a0
cogniocode <stellarandrey@gmail.com>
2020-08-20 20:42:43 +0600

    Include font

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,8 @@
     @import "~bootstrap/scss/bootstrap";
     @import "~bootstrap-vue/src";
 
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap');
+
     :root {
         --primary-gradient: #{$primary-gradient};
     }
