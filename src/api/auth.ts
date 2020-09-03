f3869c9c2bb6a817af7098cd95856fa0d9f89c5f
Andrey Chervyakov <stellarandrey@gmail.com>
2020-09-03 19:12:33 +0600

    Update auth API module to use new API client

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,21 +1,15 @@
-import Axios from "axios"
 import type {AuthCredentials} from "@/types/api/auth"
-import {ApiError, getDefaultAxiosConfig} from './util'
+import {ApiError} from './util'
+import {doRequest, RequestMethod} from "@/api/client"
 
 const AUTH_API_PATH = "/auth"
 
-export async function getAuthToken(credentials: AuthCredentials): Promise<string> {
-    try {
-        const result = await Axios.post(AUTH_API_PATH, credentials)
+export async function authenticate(credentials: AuthCredentials): Promise<string> {
+    const result = await doRequest<any>(RequestMethod.POST, AUTH_API_PATH, credentials, undefined, false)
 
-        if (result.status === 200) {
-            return result.data.token
-        } else {
-            throw new ApiError(result.data.message, result.status)
-        }
-    } catch (e) {
-        if (e.isAxiosError) {
-            throw new ApiError("Server unreachable.", 500)
-        } else throw e
+    if (result.status === 200) {
+        return result.data.token
+    } else {
+        throw new ApiError(result.data.message, result.status)
     }
 }
