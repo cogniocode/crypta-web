e4cf40d010fd345b5cd74e84eb72eaa136978a13
Andrey Chervyakov <stellarandrey@gmail.com>
2020-10-18 20:18:30 +0600

    Add settings page

---
+++ src/pages/settings/SettingsPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <page class="settings-page">
+        <app-layout>
+            
+        </app-layout>
+    </page>
+</template>
+
+<script>
+    import Page from "@/components/Page";
+    import AppLayout from "@/components/layouts/AppLayout";
+    export default {
+        name: "SettingsPage",
+        components: {AppLayout, Page}
+    }
+</script>
+
+<style scoped>
+
+</style>
