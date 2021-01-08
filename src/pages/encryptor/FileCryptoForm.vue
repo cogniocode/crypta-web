75f72d78ea490d9985d288d6a5a9f969b927c69e
Andrey Chervyakov <cognioandrey@gmail.com>
2021-01-08 19:57:27 +0600

    Update FileCryptoForm to support file encryption and decryption

--- src/pages/encryptor/FileCryptoForm.vue
+++ src/pages/encryptor/FileCryptoForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-form>
+    <b-form @submit="processFile">
         <b-form-group label="File">
             <b-form-file v-model="fields.file.value"/>
         </b-form-group>
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+    import {processFile} from "@/api/encryptor";
+
     export default {
         name: "FileCryptoForm",
         data() {
@@ -36,15 +38,37 @@
             }
         },
         methods: {
-            processFile(e) {
+            async processFile(e) {
                 if (e)
                     e.preventDefault()
 
                 try {
+                    let formData = new FormData()
 
+                    formData.append("file", this.fields.file.value)
+
+                    const blob = await processFile(formData, this.fields.operation.value)
+
+                    this.downloadFile(blob, this.fields.file.value.name, this.fields.operation.value)
                 } catch (e) {
-
+                    this.$bvToast.toast("Couldn't process file.", {
+                        title: "Error",
+                        variant: "danger",
+                        solid: "true",
+                        toaster: "b-toaster-bottom-right"
+                    })
                 }
+            },
+            downloadFile(blob, originalFilename, action) {
+                const filename = action === "ENCRYPT" ? originalFilename + ".cef" : originalFilename.split(".cef")[0]
+
+                let url = window.URL.createObjectURL(blob)
+                let link = document.createElement("a")
+                link.href = url
+                link.download = filename
+                document.body.appendChild(link)
+                link.click()
+                link.remove()
             }
         }
     }
