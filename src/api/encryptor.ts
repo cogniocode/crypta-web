2e3255c5c99e7967c790bc485005bd1debf3befd
Andrey Chervyakov <cognioandrey@gmail.com>
2021-01-08 19:57:11 +0600

    Add API for processing files

--- src/api/encryptor.ts
+++ src/api/encryptor.ts
@@ -5,9 +5,11 @@ export interface TextCryptoModel {
     text: string
 }
 
+type EncryptorAction = "ENCRYPT" | "DECRYPT"
+
 const encryptorApiPath = (type: string) => `/encryptor/${type}`
 
-export async function processText(text: TextCryptoModel, action: "ENCRYPT" | "DECRYPT"): Promise<TextCryptoModel> {
+export async function processText(text: TextCryptoModel, action: EncryptorAction): Promise<TextCryptoModel> {
     const result = await doRequest<TextCryptoModel>(RequestMethod.POST, encryptorApiPath("text"), text, {
         queryParameters: {
             action
@@ -16,6 +18,23 @@ export async function processText(text: TextCryptoModel, action: "ENCRYPT" | "DE
 
     if (result.status === 200) {
         return result.data as TextCryptoModel
+    } else {
+        throw new ApiError((result.data as ApiErrorDTO).message, result.status)
+    }
+}
+
+export async function processFile(fileForm: FormData, action: EncryptorAction): Promise<Blob> {
+    const result = await doRequest<Blob>(RequestMethod.POST, encryptorApiPath("file"), fileForm, {
+        queryParameters: {
+            action
+        }
+    }, true, {
+        responseType: "blob"
+    })
+    const filename = action === "ENCRYPT" ? (fileForm.get("file") as File).name + ".cef" : (fileForm.get("file") as File).name.split(".cef")[0]
+
+    if (result.status === 200) {
+        return result.data as Blob
     } else {
         throw new ApiError((result.data as ApiErrorDTO).message, result.status)
     }
